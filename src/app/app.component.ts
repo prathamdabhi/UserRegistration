05fb3f24d6ab9343e7b25a14613252c8f5b41d9e
prathamdabhi <prathamdabhi456@gmail.com>
2025-03-12 07:45:52 +0530

    Reactive Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {  inject, TemplateRef, ViewEncapsulation } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import Swal from 'sweetalert2';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +11,40 @@ import { Component } from '@angular/core';
   standalone: false,
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'UserRegistration';
+export class AppComponent implements OnInit {
+  
+  formgroup!:FormGroup;
+  constructor(private _toastr: ToastrService, private modalService: NgbModal,private formBuilder:FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    // Swal.fire("SweetAlert2 is working!");
+    this.setFormState()
+  }
+
+  setFormState(){
+    this.formgroup = this.formBuilder.group({
+      id: [0],
+      title: ["",Validators.required],
+      firstName: ["",Validators.required],
+      lastName:["",Validators.required],
+      email:["",[Validators.required,Validators.email]],
+      dob: ["", [Validators.required, Validators.pattern("^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-[0-9]{4}$")]],
+      password:["",[Validators.required,Validators.minLength(8)]],
+      confirmPassword:["",[Validators.required]],
+      acceptTerm: [false,Validators.requiredTrue]
+    })
+  }
+  openXl(content: TemplateRef<any>) {
+		this.modalService.open(content, { size: 'xl' });
+	}
+
+  edit(id:number){
+    alert("Edit is Called" + id)
+  }
+
+  delete(id:number){
+    alert("Delete is Called" + id)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToastrModule } from 'ngx-toastr';
+import {  NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    NgbModule,
+    ReactiveFormsModule
+    
+    
+    
   ],
   providers: [
     provideClientHydration(withEventReplay())
